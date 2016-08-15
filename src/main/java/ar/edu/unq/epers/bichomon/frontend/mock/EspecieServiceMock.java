44f0a588312130973280a7ef063093fc35f16b2b
Claudio Fernandez <claudiof@gmail.com>
2016-08-15 19:32:38 -0300

    Removed extra mock items

--- src/main/java/ar/edu/unq/epers/bichomon/frontend/mock/EspecieServiceMock.java
+++ src/main/java/ar/edu/unq/epers/bichomon/frontend/mock/EspecieServiceMock.java
@@ -20,28 +20,26 @@ public class EspecieServiceMock implements EspecieService {
 	private static Map<String, Especie> DATA = new HashMap<>();
 	
 	static {
-		for (int i = 0; i < 10; i++) {
-			Especie bohr = new Especie("Bohrmon" + i, TipoBicho.AGUA);
-			bohr.setAltura(180);
-			bohr.setPeso(75);
-			bohr.setEnergiaIncial(100);
-			bohr.setUrlFoto("https://upload.wikimedia.org/wikipedia/commons/5/5e/Niels_Bohr_Date_Unverified_LOC.jpg");
-			DATA.put(bohr.getNombre(), bohr);
-			
-			Especie schrodinger = new Especie("Schrodingermon" + i, TipoBicho.AGUA);
-			schrodinger.setAltura(170);
-			schrodinger.setPeso(69);
-			schrodinger.setEnergiaIncial(300);
-			schrodinger.setUrlFoto("https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/Erwin_Schr%C3%B6dinger_(1933).jpg/220px-Erwin_Schr%C3%B6dinger_(1933).jpg");
-			DATA.put(schrodinger.getNombre(), schrodinger);
-			
-			Especie tesla = new Especie("Teslamon" + i, TipoBicho.ELECTRICIDAD);
-			tesla.setAltura(150);
-			tesla.setPeso(55);
-			tesla.setEnergiaIncial(5000);
-			tesla.setUrlFoto("https://upload.wikimedia.org/wikipedia/commons/7/79/Tesla_circa_1890.jpeg");
-			DATA.put(tesla.getNombre(), tesla);
-		}
+		Especie bohr = new Especie("Bohrmon", TipoBicho.AGUA);
+		bohr.setAltura(180);
+		bohr.setPeso(75);
+		bohr.setEnergiaIncial(100);
+		bohr.setUrlFoto("https://upload.wikimedia.org/wikipedia/commons/5/5e/Niels_Bohr_Date_Unverified_LOC.jpg");
+		DATA.put(bohr.getNombre(), bohr);
+		
+		Especie schrodinger = new Especie("Schrodingermon", TipoBicho.AGUA);
+		schrodinger.setAltura(170);
+		schrodinger.setPeso(69);
+		schrodinger.setEnergiaIncial(300);
+		schrodinger.setUrlFoto("https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/Erwin_Schr%C3%B6dinger_(1933).jpg/220px-Erwin_Schr%C3%B6dinger_(1933).jpg");
+		DATA.put(schrodinger.getNombre(), schrodinger);
+		
+		Especie tesla = new Especie("Teslamon", TipoBicho.ELECTRICIDAD);
+		tesla.setAltura(150);
+		tesla.setPeso(55);
+		tesla.setEnergiaIncial(5000);
+		tesla.setUrlFoto("https://upload.wikimedia.org/wikipedia/commons/7/79/Tesla_circa_1890.jpeg");
+		DATA.put(tesla.getNombre(), tesla);
 	}
 	
 	@Override
