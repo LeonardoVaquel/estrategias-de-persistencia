f96f8876406076071dfb27a0050e9dadea0d6ee4
gisele <gisele@gisele-Inspiron-1545>
2016-09-20 19:44:39 -0300

    Ubicación
    +se implementa dojo, guarderia, pueblo, ubicación.

---
+++ src/main/java/ar/edu/unq/epers/bichomon/backend/model/ubicacion/Dojo.java
@@ -0,0 +1,19 @@
+package ar.edu.unq.epers.bichomon.backend.model.ubicacion;
+
+import ar.edu.unq.epers.bichomon.backend.model.entrenador.Entrenador;
+
+public class Dojo extends Ubicacion {
+	
+	public Dojo() {
+		super();
+			}
+
+	@Override
+	public void buscar(Entrenador entrenador) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+
+}
--- src/main/java/ar/edu/unq/epers/bichomon/backend/model/ubicacion/Ubicacion.java
+++ src/main/java/ar/edu/unq/epers/bichomon/backend/model/ubicacion/Ubicacion.java
@@ -1,5 +1,10 @@
 package ar.edu.unq.epers.bichomon.backend.model.ubicacion;
 
-public class Ubicacion {
+import ar.edu.unq.epers.bichomon.backend.model.entrenador.Entrenador;
 
+public abstract class Ubicacion {
+
+	private String nombre;
+
+	public abstract void buscar(Entrenador entrenador);
 }
